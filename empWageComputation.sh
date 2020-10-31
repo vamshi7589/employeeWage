79bb4f66df9a72fe6263f5f35ab67b27aa325251
vamshi7589 <vamshi7589@gmail.com>
2020-10-31 23:42:27 +0530

    added part time employee wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -11,3 +11,28 @@ then
 else
 	echo "Employee Absent"
 fi
+#TO FUNCTION CALCULATE EMPLOYEE WAGE
+function calculateDailyEmployeeWae(){
+	read -p "Assume Wage per hour is :" isWagePerHour "rupees"
+   isDayHour=24
+ 	read -p "To full day hours is :" isFullDayHour
+	calculateEmployeeWage=$(( $isWagePerHour * $isFullDayHour ))
+	echo "To calculate daily employee wage :" $calculateEmployeeWage "rupees"
+}
+if [ $randomNumber -eq 	$isPresent ]
+then
+	echo "Employee Present"
+  	#TO FUNCTION CALCULATE EMPLOYEE WAGE
+	function calculateDailyEmployeeWae(){
+  	#TO FUNCTION PART TIME EMPLOYEE AND  WAGE
+	function partTimeEmployeeAndWage(){
+	read -p "Assume Wage per hour is :" isWagePerHour "rupees"
+        isDayHour=24
+ 	read -p "To full day hours is :" isFullDayHour
+	calculateEmployeeWage=$(( $isWagePerHour * $isFullDayHour ))
+	echo "To calculate daily employee wage :" $calculateEmployeeWage "rupees"
+	isDayHour=24
+ 	read -p "To part time hour is :" isFullDayHour
+	calculatePartTimeEmployeeWage=$(( $isWagePerHour * $isFullDayHour ))
+	echo "To calculate part time employee and wage :" $calculatePartTimeEmployeeWage "rupees"
+}
