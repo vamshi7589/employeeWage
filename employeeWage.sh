af55b25cac88ca2a7813284ad21d90901df9179b
vamshi7589 <vamshi7589@gmail.com>
2020-10-31 23:23:42 +0530

    added employee wage

--- employeeWage.sh
+++ employeeWage.sh
@@ -10,3 +10,11 @@ then
 else
 	echo "Employee Absent"
 fi
+#TO FUNCTION CALCULATE EMPLOYEE WAGE
+function calculateDailyEmployeeWae(){
+	read -p "Assume Wage per hour is :" isWagePerHour "rupees"
+   isDayHour=24
+ 	read -p "To full day hours is :" isFullDayHour
+	calculateEmployeeWage=$(( $isWagePerHour * $isFullDayHour ))
+	echo "To calculate daily employee wage :" $calculateEmployeeWage "rupees"
+}
